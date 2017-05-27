a2e71cbb9520e070cebfdc1a18f7e72338f8d128
andrianna <andrianna.jack@gmail.com>
2017-05-27 05:31:57 +0000

    unfinished

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,26 @@
 from flask import Flask
-
+from app import views, models
+from flasksqlalchemy import SQLAlchemy
+import logging
+import sys
 app = Flask(__name__)
-from app import views
+
+
+app.config['SECRET_KEY'] = "$$tarz"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password@localhost/project1"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+UPLOAD_FOLDER = './app/static/uploads'
+
+db = SQLAlchemy(app)
+ 
+app.config.from_object(__name__)
+
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+
+
+app.config.from_object(__name__)
+from app import views, models
